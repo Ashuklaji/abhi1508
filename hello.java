b9dab4d0527d828d0cd9cc43cb5d46b41f1e9041
Ashuklaji <92952180+Ashuklaji@users.noreply.github.com>
2022-12-09 17:52:35 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,18 @@
+package firstproj;
+
+import java.util.*;
+
+public class hello {
+	public static void main(String args[]) {
+	for(int i =1;i<21;i++) {
+		if(i % 2 !=0) {
+			System.out.println("odd number is"+i);
+		}
+		
+	}
+	
+	
+	 
+	  }
+}
+		
